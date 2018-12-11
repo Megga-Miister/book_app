b7e67e7efea64248ef5714c2adee4ebb5384b1e5
ntibbals <ntibbals@outlook.com>
2018-12-11 10:02:30 -0800

    server rendering

--- server.js
+++ server.js
@@ -8,17 +8,15 @@ app.set('view engine', 'ejs');
 app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('../views/pages/index');
 });
 
-app.get('/views/pages/index', (req, res) => {
-  res.render('index', {});
+app.get('/', (req, res) => {
+  res.render('../views/pages/error');
 });
-app.get('/views/pages/error', (req, res) => {
-  res.render('index', {});
-});
-app.get('/views/pages/searches/show', (req, res) => {
-  res.render('index', {});
+
+app.get('/', (req, res) => {
+  res.render('../views/pages/searches/show');
 });
 
 app.listen(PORT, () => {
