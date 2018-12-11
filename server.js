997b6c002dbe1eb90082da4d76b085a288214119
ntibbals <ntibbals@outlook.com>
2018-12-11 10:51:21 -0800

    completed feature 2

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 app.use(express.static('./public'));
 
 app.get('/hello', (req, res) => {
@@ -17,6 +18,11 @@ app.get('/', (req, res) => {
 
 app.get('/', (req, res) => {
   res.render('../views/pages/searches/show');
+});
+
+app.post('/search', (req, res) => {
+  console.log('search criteria', req.body);
+  // res.sendFile('/searches/show', {root: './public'});
 });
 
 app.listen(PORT, () => {
