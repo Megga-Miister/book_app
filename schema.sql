a913e01f96c934eeb8c9400640da59bda4e1efac
ntibbals <ntibbals@outlook.com>
2018-12-13 11:54:45 -0800

    completed db

--- schema.sql
+++ schema.sql
@@ -11,6 +11,3 @@ CREATE TABLE books (
   img_url VARCHAR (255),
   bookshelf VARCHAR (255)
 );
-
-INSERT INTO books (title, author, isbn, description, img_url, bookshelf) VALUES ('Frank', 'the tank', '87798', 'Just one', 'www.img.com', 'shelf');
-INSERT INTO books (title, author, isbn, description, img_url, bookshelf) VALUES ('Crank', 'Soldier boy', '87798', 'Just two', 'www.img.com', 'shelf2');
