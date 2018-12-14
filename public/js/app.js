ece703ed6c6e554cf497ab7dac4ee0f34fe99bc6
ntibbals <ntibbals@outlook.com>
2018-12-13 22:29:52 -0800

    working on hamburger

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,26 @@
 $('.selectbut').on('click', function() {
   $('.hidden').toggleClass('hidden');
 });
+
+	// Toggle.
+	$(
+		'<div id="headerToggle">' +
+		'<a href="#header" class="toggle"></a>' +
+		'</div>'
+	)
+		.appendTo($body);
+
+	// Header.
+	$('#header')
+		.panel({
+			delay: 500,
+			hideOnClick: true,
+			hideOnSwipe: true,
+			resetScroll: true,
+			resetForms: true,
+			side: 'left',
+			target: $body,
+			visibleClass: 'header-visible'
+		});
+
+})(jQuery);
